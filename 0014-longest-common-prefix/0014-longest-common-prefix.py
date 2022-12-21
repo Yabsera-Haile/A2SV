192db566852cb43ac718cdf07a781f97cd5de417
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2022-12-21 19:16:09 +0300

    Time: 80 ms (18.84%), Space: 14 MB (12.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,22 +1,38 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        stack=[]
-        flag=0
-        for i in range(200):
-            for j in range(len(strs)):
-                if i>=len(strs[j]):
-                    if stack and j!=0: 
-                        stack.pop()
-                    flag=flag+1
+    def longestCommonPrefix(self, strs: List[str]) -> str: 
+        short = min(strs, key=len) 
+        for item in strs: 
+            while len(short) > 0:
+                if item.startswith(short):
                     break
-                elif j==0: 
-                    stack.append(strs[j][i])
-                elif strs[j][i]!=stack[-1]:
-                    if stack:
-                        stack.pop()
-                    flag=flag+1
-                    break
-            if flag>=1:
-                break
-        return "".join(stack)
+                else:
+                    short = short[:-1] 
+        return short
+
+
+
+
+
+# Brute Force
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str]) -> str:
+#         stack=[]
+#         flag=0
+#         for i in range(len(strs[0])):
+#             for j in range(len(strs)):
+#                 if i>=len(strs[j]):
+#                     if stack and j!=0: 
+#                         stack.pop()
+#                     flag=flag+1
+#                     break
+#                 elif j==0: 
+#                     stack.append(strs[j][i])
+#                 elif strs[j][i]!=stack[-1]:
+#                     if stack:
+#                         stack.pop()
+#                     flag=flag+1
+#                     break
+#             if flag>=1:
+#                 break
+#         return "".join(stack)
                     
