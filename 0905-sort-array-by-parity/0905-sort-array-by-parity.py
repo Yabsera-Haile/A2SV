e8651e2556ae448fd97349d560350301a98ef9e1
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2022-12-26 14:51:57 +0300

    Time: 104 ms (69.79%), Space: 14.7 MB (60.15%) - LeetHub

---
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.py
@@ -0,0 +1,32 @@
+class Solution:
+    #check weather even or odd
+    def checkParity(self, num: int)-> bool:
+        parity=False
+        if num%2==0:
+            parity=True
+        return parity 
+    
+    
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        #initialize pointers
+        left=0
+        right=1
+        
+        #swap all even numbers
+        while right < len(nums):
+            if self.checkParity(nums[left]) and self.checkParity(nums[right]):
+                left+=1
+                right+=1
+            elif self.checkParity(nums[left])==False and self.checkParity(nums[right]):
+                nums[left],nums[right]=nums[right],nums[left]
+                left+=1
+                right+=1
+            elif self.checkParity(nums[left])==False and self.checkParity(nums[right])==False:
+                right+=1
+            else:
+                left+=1
+                right+=1
+        
+        return nums
+                
+                
