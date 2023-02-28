9a982cd6932191332dbe9b3524fb243670394e19
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-02-28 16:06:12 +0300

    Time: 199 ms (77.90%), Space: 43.2 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,12 +3,29 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        left=0
-        right=len(s)-1
-        
-        while left < right:
-            s[left], s[right] = s[right], s[left]
-            left+=1
-            right-=1
+        def helperFunc(left,right):
+            if left<right:
+                s[left], s[right] = s[right], s[left]
+                helperFunc(left+1,right-1)
+            else:
+                return s
             
-        return s
+        return helperFunc(0,len(s)-1)
+
+        
+        
+        
+        
+        
+        
+        
+        
+#         left=0
+#         right=len(s)-1
+        
+#         while left < right:
+#             s[left], s[right] = s[right], s[left]
+#             left+=1
+#             right-=1
+            
+#         return s
