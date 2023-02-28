469c43d905b52f3d5b02a65bd7462c149c2d8761
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-02-28 16:47:12 +0300

    Time: 39 ms (65.72%), Space: 13.9 MB (23.48%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -6,13 +6,47 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         result=temp=ListNode()
-        while list1 and list2:
-            if list1.val<list2.val:
+        
+        def helperFunc(list1,list2,result):
+            if list1 and list2:
+                if list1.val<list2.val:
+                    result.next=list1
+                    helperFunc(list1.next,list2,result.next)
+                else:
+                    result.next=list2
+                    helperFunc(list1,list2.next,result.next)
+            elif list1:
                 result.next=list1
-                list1,result=list1.next,result.next
-            else:
+                helperFunc(list1.next,list2,result.next)
+            elif list2:
                 result.next=list2
-                list2,result=list2.next,result.next
-        if list1 or list2:
-            result.next=list1 if list1 else list2
-        return temp.next
+                helperFunc(list1,list2.next,result.next)
+            else:
+                return result
+        
+        result=helperFunc(list1,list2,result)
+        return temp.next        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # result=temp=ListNode()
+        # while list1 and list2:
+        #     if list1.val<list2.val:
+        #         result.next=list1
+        #         list1,result=list1.next,result.next
+        #     else:
+        #         result.next=list2
+        #         list2,result=list2.next,result.next
+        # if list1 or list2:
+        #     result.next=list1 if list1 else list2
+        # return temp.next
