b43dc978c78c425fb8a910f639f088a70d493271
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-03-01 11:21:59 +0300

    Time: 4237 ms (15.09%), Space: 14.1 MB (40.30%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -3,32 +3,32 @@ class Solution:
         def helperFunc(left,right,turn):
             if left==right:
                 if turn==1:
-                    return [nums[left],0]
+                    return nums[left]-0
                 elif turn==2:
-                    return [0,nums[left]]
+                    return 0-nums[left]
             else:
                 if turn == 1:
                     ans1=helperFunc(left+1,right,2)
-                    ans1[0]+=nums[left]
+                    ans1+=nums[left]
                     ans2=helperFunc(left,right-1,2)
-                    ans2[0]+=nums[right]
-                    if ans1[0]>=ans2[0]:
+                    ans2+=nums[right]
+                    if ans1>=ans2:
                         return ans1
                     else:
                         return ans2
                 elif turn ==2:
                     ans1=helperFunc(left+1,right,1)
-                    ans1[1]+=nums[left]
+                    ans1-=nums[left]
                     ans2=helperFunc(left,right-1,1)
-                    ans2[1]+=nums[right]
-                    if ans1[1]>=ans2[1]:
+                    ans2-=nums[right]
+                    if ans1<ans2:
                         return ans1
                     else:
                         return ans2
         result=helperFunc(0,len(nums)-1,1)
             
             
-        if result[0]>=result[1]:
+        if result>=0:
             return True
         else:
             return False
