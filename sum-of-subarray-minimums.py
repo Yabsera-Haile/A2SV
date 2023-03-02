a2943c979da0dac180fa16068aca90893f1a1ced
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-03-02 18:16:27 +0300

    added sum-of-subarray-minimums.python

---
+++ sum-of-subarray-minimums.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        arr=[0]+arr+[0]
+        stack=[]
+        result=0
+
+        for i in range(len(arr)):
+            while stack and arr[i]<arr[stack[-1]]:
+                curr=stack.pop()
+                result+=arr[curr]*(i-curr)*(curr-stack[-1])
+            stack.append(i)
+
+        return result % (10**9+7)
