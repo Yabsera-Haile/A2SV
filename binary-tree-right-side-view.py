7a95569d02f9b63a799c68a89b15b7c921dae71b
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-03-09 16:10:52 +0300

    added binary-tree-right-side-view.python

---
+++ binary-tree-right-side-view.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        result=defaultdict(list)
+
+        def helperFunc(root,depth):
+            if root:
+                curr=[]
+                if root.left:
+                    curr.append(helperFunc(root.left,depth+1))
+                if root.right:
+                    curr.append(helperFunc(root.right,depth+1))
+                if len(curr)>0:
+                    result[depth+1].extend(curr)
+                
+                return root.val
+        
+        first=helperFunc(root,0)
+        result[0].append(first)
+        myKeys = list(result.keys())
+        myKeys.sort()
+        sorted_dict = {i: result[i] for i in myKeys}
+        answer=[res[-1] for res in sorted_dict.values()]
+        return answer if root else []
