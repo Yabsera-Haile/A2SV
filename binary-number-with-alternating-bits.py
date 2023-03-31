31a00961f842823f8f3fd33eee50ba9df4615cbc
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-03-31 17:24:44 +0300

    added binary-number-with-alternating-bits.python

---
+++ binary-number-with-alternating-bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        
+        for i in range(1,n.bit_length()):
+            if ((n & (1<<i)) ==0 and (n & (1<<(i-1)))==0) or ((n & (1<<i)) !=0 and (n & (1<<(i-1)))!=0) :
+                return False
+        return True
