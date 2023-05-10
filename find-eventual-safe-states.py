db153945ce3d9a3523a02d6b4690f5f9bb82dbe4
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-05-10 18:23:43 +0300

    added find-eventual-safe-states.python

--- find-eventual-safe-states.py
+++ find-eventual-safe-states.py
@@ -1,24 +1,55 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        queue=deque()
-        dep=defaultdict(list)
-        count=defaultdict(int)
+        # DFS     
+        result=[]
+        color=[0]*len(graph)
+
+        def dfs(vertex):
+            if color[vertex]==2:
+                return True
+            if color[vertex]==1:
+                return False
+            color[vertex]=1
+
+            for neigh in graph[vertex]:
+                curr=dfs(neigh)
+                if not curr:
+                    return False
+                
+            result.append(vertex)
+            color[vertex]=2
+            return True
 
         for i in range(len(graph)):
-            if len(graph[i])==0:
-                queue.append(i)
-            for j in range(len(graph[i])):
-                dep[graph[i][j]].append(i)
-                count[i]+=1
-        
-        result=[]
-        while queue:
-            curr=queue.popleft()
-            result.append(curr)
-
-            for neigh in dep[curr]:
-                count[neigh]-=1
-                if count[neigh]==0:
-                    queue.append(neigh)
+            if color[i]==0:
+                dfs(i)
         
         return sorted(result)
+
+
+
+
+
+        # BFS
+        # queue=deque()
+        # dep=defaultdict(list)
+        # count=defaultdict(int)
+
+        # for i in range(len(graph)):
+        #     if len(graph[i])==0:
+        #         queue.append(i)
+        #     for j in range(len(graph[i])):
+        #         dep[graph[i][j]].append(i)
+        #         count[i]+=1
+        
+        # result=[]
+        # while queue:
+        #     curr=queue.popleft()
+        #     result.append(curr)
+
+        #     for neigh in dep[curr]:
+        #         count[neigh]-=1
+        #         if count[neigh]==0:
+        #             queue.append(neigh)
+        
+        # return sorted(result)
