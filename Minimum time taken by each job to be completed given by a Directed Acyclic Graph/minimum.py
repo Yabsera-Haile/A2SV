4a5842a7e98e3336aac1f50815e3f20576316743
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-05-12 17:47:54 +0300

    added Minimum time taken by each job to be completed given by a Directed Acyclic Graph/minimum.py

---
+++ Minimum time taken by each job to be completed given by a Directed Acyclic Graph/minimum.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+from typing import List
+from collections import defaultdict,deque
+
+class Solution:
+    def minimumTime(self, n : int,m : int, edges : List[List[int]]) -> int:
+        # code here
+        graph=defaultdict(list)
+        count=defaultdict(int)
+        
+        for u,v in edges:
+            graph[u].append(v)
+            count[v]+=1
+        queue=deque()
+        for i in range(1,n+1):
+            if count[i]==0:
+                queue.append(i)
+        print(queue)
+        result=[]
+        time=1
+        while queue:
+            l=len(queue)
+            for _ in range(l):
+                curr=queue.popleft()
+                result.append(time)
+                for neigh in graph[curr]:
+                    count[neigh]-=1
+                    if count[neigh]==0:
+                        queue.append(neigh)
+            time+=1
+        
+        return result
