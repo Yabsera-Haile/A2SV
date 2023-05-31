d6e74638cd2ef6acd68166e83a696a436b84667e
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-05-31 19:38:46 +0300

    added unique-paths.python

---
+++ unique-paths.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo=defaultdict(int)
+        memo[(m-1,n-1)]=1
+
+        def check(row,col):
+            if 0<=row<m and 0<=col<n:
+                return memo[(row,col)]
+            return 0
+        
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                memo[(i,j)]+=check(i+1,j)+check(i,j+1)
+        
+        return memo[(0,0)]
