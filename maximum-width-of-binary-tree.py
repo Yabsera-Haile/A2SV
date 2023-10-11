0f206f1ea66c5ee704fadc7bb9204c1b781b1a1c
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-10-11 16:41:30 +0300

    added maximum-width-of-binary-tree.python

--- maximum-width-of-binary-tree.py
+++ maximum-width-of-binary-tree.py
@@ -6,18 +6,37 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        count=defaultdict(list)
-        result=0
-        def helperFunc(root,depth,val):
-            if root:
-                count[depth].append(val)
+        # BFS
+        result=1
+        queue=deque([(root,0)])
 
-                helperFunc(root.left,depth+1,2*val)
-                helperFunc(root.right,depth+1,2*val+1)
-        
-        helperFunc(root,0,1)
-        for key,value in count.items():
-            curr=value[-1]-value[0]+1
-            result=max(result,curr)
+        while queue:
+            l=len(queue)
+            result=max(result,queue[-1][1]-queue[0][1]+1)
+
+            for _ in range(l):
+                node,level = queue.popleft()
+                if node.left:
+                    queue.append((node.left,level*2+1))
+                if node.right:
+                    queue.append((node.right,level*2+2))
         
         return result
+
+
+        # DFS
+        # count=defaultdict(list)
+        # result=0
+        # def helperFunc(root,depth,val):
+        #     if root:
+        #         count[depth].append(val)
+
+        #         helperFunc(root.left,depth+1,2*val)
+        #         helperFunc(root.right,depth+1,2*val+1)
+        
+        # helperFunc(root,0,1)
+        # for key,value in count.items():
+        #     curr=value[-1]-value[0]+1
+        #     result=max(result,curr)
+        
+        # return result
