c946bff328d2d910844c535040abd9dcb0ee4062
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-10-16 16:11:24 +0300

    added powx-n.python

--- powx-n.py
+++ powx-n.py
@@ -1,13 +1,25 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def helperFunc(x,n):
-            if n==1 or n==0 or n==-1:
-                return x**n
-            else:
-                if n%2==0:
-                    return helperFunc(x,n//2) **2
-                else:
-                    return helperFunc(x,n//2) **2*x
+        # Binary Exponentiation
+        neg=n>0
+        n=abs(n)
+        result=1
+        while n>0:
+            if n & 1:
+                result*=x
+            x*=x
+            n>>=1
+        return result if neg else 1/result
+
+        # Recursive
+        # def helperFunc(x,n):
+        #     if n==1 or n==0 or n==-1:
+        #         return x**n
+        #     else:
+        #         if n%2==0:
+        #             return helperFunc(x,n//2) **2
+        #         else:
+        #             return helperFunc(x,n//2) **2*x
 
 
-        return helperFunc(x,n)
+        # return helperFunc(x,n)
