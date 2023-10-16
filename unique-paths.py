67cf21b27d3df7dc6f7aaf9c677d7f49aa20d4c4
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-10-16 15:16:59 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,15 +1,20 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        memo=defaultdict(int)
-        memo[(m-1,n-1)]=1
+        # Combinatorics
+        total=m+n-2
+        return factorial(total)//(factorial(total-(m-1)) * factorial(m-1))
+        
+        # DP
+        # memo=defaultdict(int)
+        # memo[(m-1,n-1)]=1
 
-        def check(row,col):
-            if 0<=row<m and 0<=col<n:
-                return memo[(row,col)]
-            return 0
+        # def check(row,col):
+        #     if 0<=row<m and 0<=col<n:
+        #         return memo[(row,col)]
+        #     return 0
         
-        for i in range(m-1,-1,-1):
-            for j in range(n-1,-1,-1):
-                memo[(i,j)]+=check(i+1,j)+check(i,j+1)
+        # for i in range(m-1,-1,-1):
+        #     for j in range(n-1,-1,-1):
+        #         memo[(i,j)]+=check(i+1,j)+check(i,j+1)
         
-        return memo[(0,0)]
+        # return memo[(0,0)]
