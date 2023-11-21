538bab19a744a723737944c9077cfacd1d9db95d
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2023-11-21 19:25:58 +0300

    Added solution for Valid Parentheses

---
+++ leetcode-solutions/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        result=[]
+        closers={"}":"{",
+                 ")":"(",
+                 "]":"[",}
+        for letter in s:
+            if letter in closers:
+                if result and result[-1]==closers[letter]:
+                    result.pop()
+                else:
+                    return False
+            else:
+                result.append(letter)
+        return True if not result else False
+        
+        
