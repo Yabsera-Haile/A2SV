d819f8cfe4f1e29306945ebff790c9f7a4ec665e
Yabsera Haile Yemanberhan <83125525+Yabsera-Haile@users.noreply.github.com>
2024-04-09 13:33:23 +0300

    Added solution for Time Needed to Buy Tickets

---
+++ leetcode-solutions/time-needed-to-buy-tickets.py
@@ -0,0 +1,16 @@
+from collections import deque
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time=0
+
+        while tickets[k]>0:
+            for i in range(len(tickets)):
+                if tickets[i] > 0:
+                    time+=1
+                    tickets[i]-=1
+                if tickets[i]==0 and i==k:
+                    break
+
+                    
+        return time
+
